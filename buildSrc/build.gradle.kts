6f55b972c31663686a03f8283bcd4df065179ef6
Mahdi Mohammadi <mahdi60mm60@gmail.com>
2024-06-07 15:58:11 +0330

    change

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,12 @@
+import org.gradle.kotlin.dsl.`kotlin-dsl`
+
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    google()
+    mavenCentral()
+    maven(url = "https://jitpack.io")
+    maven("https://plugins.gradle.org/m2/")
+}
